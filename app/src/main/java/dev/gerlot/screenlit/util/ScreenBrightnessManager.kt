0750f3dd9e76994a61e12939adffa1c43d91f676
Balázs Gerlei <gerlotdev@gmail.com>
2024-03-27 10:10:54 +0100

    Extract screen brightness calculation logic to a new ScreenBrightnessManager class to be able to work on it without conflicts with logic in ScreenLitActivity

---
+++ app/src/main/java/dev/gerlot/screenlit/util/ScreenBrightnessManager.kt
@@ -0,0 +1,55 @@
+package dev.gerlot.screenlit.util
+
+import kotlin.math.abs
+
+class ScreenBrightnessManager {
+
+    private var screenBrightnessChangeStart: Float? = null
+    private var screenBrightnessAtChangeStart: Float? = null
+
+    val changingBrightness: Boolean
+        get() = screenBrightnessChangeStart != null
+
+    fun onStartScreenBrightnessChange(startCoordinate: Float, currentScreenBrightness: Float?) {
+        currentScreenBrightness?.let { brightness ->
+            screenBrightnessChangeStart = startCoordinate
+            screenBrightnessAtChangeStart = Math.round(brightness * 1000f) / 1000f
+        }
+    }
+
+    fun onScreenBrightnessChangeRequest(viewHeight: Int, y: Float, onChangeScreenBrightness: (newBrightness: Float) -> Unit) {
+        screenBrightnessChangeStart?.let { start ->
+            val normalizedStart = calculateNormalizedScreenPosition(start, viewHeight)
+            if (!isSmallMove(start, y, viewHeight)) { // Ignore small movement that can be an imprecise tap
+                val normalizedScreenPosition = calculateNormalizedScreenPosition(y, viewHeight)
+                val screenBrightnessChange = Math.round((normalizedScreenPosition - normalizedStart) * 1000f) / 1000f * 2f
+
+                val previousBrightness = screenBrightnessAtChangeStart
+                previousBrightness?.let {
+                    val newBrightness = (Math.round((it + screenBrightnessChange) * 1000f) / 1000f).coerceIn(0f, 1f)
+                    onChangeScreenBrightness(newBrightness)
+                }
+            }
+        }
+    }
+
+    fun onEndScreenBrightnessChange() {
+        screenBrightnessChangeStart = null
+        screenBrightnessAtChangeStart = null
+    }
+
+    private fun calculateNormalizedScreenPosition(y: Float, viewHeight: Int) = Math.round(1f.minus(Math.round((y / viewHeight) * 1000f) / 1000f) * 1000f) / 1000f
+
+    private fun isSmallMove(start: Float, y: Float, viewHeight: Int): Boolean {
+        val distance = abs(start - y)
+        val threshold = viewHeight / CHANGE_THRESHOLD_DIVISOR
+        return distance < threshold
+    }
+
+    companion object {
+
+        private const val CHANGE_THRESHOLD_DIVISOR = 160f
+
+    }
+
+}
